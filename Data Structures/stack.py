7f7b8d423f4b1058a105f5b892222cfbe993a023
sagniknayak <sagniknayak26@gmail.com>
2021-03-29 12:18:19 +0530

    changes in stack.py

--- Data Structures/stack.py
+++ Data Structures/stack.py
@@ -21,36 +21,64 @@ class Stack:
             boolean: Indicates whether the Stack is overflowing.
         '''
         return self._top == self._max_size - 1 if self._flag_overflow else False
+
     
-    def __check_underflow__(self):
+    def empty(self):
         '''
-        __check_underflow__ Checks if the Stack is underflowing.
+        empty Checks whether the Stack is empty.
 
         Returns:
-            boolean: Indicates whether the Stack is underflowing.
+            boolean: Returns whether the Stack is empty.
         '''
         return self._top == -1
 
+    
+    def size(self):
+        '''
+        size Returns the size of the Stack.
+
+        Returns:
+            int: Size of the Stack.
+        '''
+        return self._top + 1
+
+    
+    def peek(self):
+        '''
+        peek Peeks into the Stack.
+
+        Returns:
+            (<Stack Element>):  Top Element of the Stack.
+        '''
+        return self._stack[self._top] if not self.empty() else None
+
+    
     def push(self, element):
         '''
         push Pushes element into the Stack.
 
         Args:
-            element (Any): This element is pushed on the top of the Stack.
+            element (<Stack Element>): Element to be pushed into the Stack.
+
+        Returns:
+            boolean: Conveys if the element is pushed successfully or not.
         '''
         if not self.__check_overflow__():
             self._stack += [element]
             self._top += 1
+            return True
         else:
-            print("Stack Overflowing")
+            return False
 
     def pop(self):
         '''
-        pop Pops the topmost element of the Stack.
+        pop Pops the top element of the Stack.
+
+        Returns:
+            <Stack Element>: The topmost element popped off the Stack.
         '''
-        if not self.__check_underflow__():
-            print(self._stack[self._top])
+        if not self.empty():
+            val = self._stack[self._top]
             del self._stack[self._top]
             self._top -= 1
-        else:
-            print("Stack Underflowing")
+            return val
